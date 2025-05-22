1a10542b3d3024e57c2375a66eaa42d93182f730
LeandroFFBR <leandrofelixf@outlook.com>
2025-05-21 23:12:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import flet as ft
 from tela_login import TelaLogin
 
 def main(page: ft.Page):
-    page.window.frameless= True
-    page.window.maximized= True
-    page.window.full_screen= False
-    page.title = "Longano"
     
     # Inicializa a tela de login
     TelaLogin(page)
